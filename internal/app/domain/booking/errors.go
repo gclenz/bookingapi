f54921d1659100b2a3a6b6187af5eeb6bcdc7a3c
Gabriel Lenz <gabrielclenz@gmail.com>
2025-01-22 23:36:26 -0300

    refactor: error messages

--- internal/app/domain/booking/errors.go
+++ internal/app/domain/booking/errors.go
@@ -3,9 +3,9 @@ package booking
 import "errors"
 
 var (
-	ErrOverlappingBooking = errors.New("there is already a booking between the chosen dates")
+	ErrOverlappingBooking = errors.New("there is already a booking between the chosen dates and room")
 	ErrCreateBooking      = errors.New(
-		"something went wrong while trying to create a new booking. Please, try again. If the error persists, contact the development team",
+		"something went wrong while trying to create a new booking. Please, try again. If the error persists, contact the support",
 	)
 	ErrBookingInThePast         = errors.New("you can't book a room in the past")
 	ErrEndDateBeforeStartAtDate = errors.New("you can't have an end date before the start date")
